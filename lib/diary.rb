c4ea3917557216b78410ea7ed326b330961258bd
Ara <hello.ara@pm.me>
2020-12-18 15:10:11 +0000

    refactor

--- lib/diary.rb
+++ lib/diary.rb
@@ -21,31 +21,26 @@ class Diary
   end
 
   def self.find(id:)
-    connection = PG.connect(dbname: 'diary')
-    result = connection.exec("SELECT * FROM diary WHERE id=#{id}") 
+    result = DatabaseConnection.query("SELECT * FROM diary WHERE id=#{id}") 
     Diary.new(id: result[0]['id'], title: result[0]['title'], entry:result[0]['entry'])
   end
 
   def self.edit(id: , title: , entry: )
-    connection = PG.connect(dbname: 'diary')
-    result = connection.exec("UPDATE diary SET title='#{title}', entry='#{entry}' WHERE id=#{id} RETURNING id, title, entry;")
+    result = DatabaseConnection.query("UPDATE diary SET title='#{title}', entry='#{entry}' WHERE id=#{id} RETURNING id, title, entry;")
     Diary.new(id: result[0]['id'], title: result[0]['title'], entry: result[0]['entry'])
   end
 
   def self.delete(id:)
-    connection = PG.connect(dbname: 'diary')
-    result = connection.exec("DELETE FROM diary WHERE id=#{id}")
+    DatabaseConnection.query("DELETE FROM diary WHERE id=#{id}")
   end
 
   def comments(diary_id: )
-    connection = PG.connect(dbname: 'diary')
-    result = connection.query("SELECT * FROM comments where diary_id=#{diary_id}")
+    result = DatabaseConnection.query("SELECT * FROM comments where diary_id=#{diary_id}")
     result.map{ |comment| Comment.new(id: comment['id'], comment: comment['comment'], diary_id: comment['diary_id'])}
   end
 
   def self.add_comments(diary_id: , comment: )
-    connection = PG.connect(dbname: 'diary')
-    result = connection.query("INSERT INTO comments(diary_id, comment) VALUES('#{diary_id}', '#{comment}') RETURNING id, comment, diary_id")
+    result = DatabaseConnection.query("INSERT INTO comments(diary_id, comment) VALUES('#{diary_id}', '#{comment}') RETURNING id, comment, diary_id")
     Comment.new(id: result[0]['id'], comment: result[0]['comment'], diary_id:result[0]['diary_id'])
   end
 
